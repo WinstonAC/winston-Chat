908a1c0a4b2bbcbe0759c03a333f0a42ae22c1ea
Billy_Cambell <Billy.campbell17@gmail.cOM>
2025-05-07 22:33:02 +0200

    fix: update X-Frame-Options header for Vercel compatibility

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/winston-widget.html',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM *'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors *"
+          }
+        ],
+      },
+    ];
+  },
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
